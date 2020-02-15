1d98aa4fd35c6fa5e3e2583b2da19e879fe2f5ce
Giovanni Garufi <giovanni@balena.io>
2020-02-15 20:48:29 +0000

    Remove unused packages
    Change-type: patch
    Signed-off-by: Giovanni Garufi <giovanni@balena.io>

--- test/test.js
+++ test/test.js
@@ -5,6 +5,5 @@ const fs = require('fs')
 test('Parse CODEOWNERS example', t => {
 	const codeowners = fs.readFileSync(__dirname + '/CODEOWNERS.test', 'utf8')
 	const parsedCodeowners = JSON.parse(fs.readFileSync(__dirname + '/CODEOWNERS.json'))
-	console.log(parse(codeowners))
 	t.deepEqual(parse(codeowners), parsedCodeowners)
 });
